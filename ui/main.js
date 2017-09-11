6f530630724dea21f34772ffd106ca5c1bc9d02a
pavan879 <30998102+pavan879@users.noreply.github.com>
2017-09-11 19:49:35 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,1 +1,17 @@
 console.log('Loaded!');
+
+function main() {
+  $('.skillset').hide();
+  $('.skillset').fadeIn(1000);
+  $('.projects').hide();
+  $('.projects-button').on('click',function()
+                   {
+    $(this).next().slideToggle(400);
+    $(this).text('Projects Viewed');
+    
+  });
+ 
+  
+}
+
+$(document).ready(main);
