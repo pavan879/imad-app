06f3cbed5075864d61a333e6be8f69107a20275d
pavan879 <30998102+pavan879@users.noreply.github.com>
2017-09-11 21:45:03 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -2,24 +2,6 @@ var express = require('express');
 var morgan = require('morgan');
 var path = require('path');
 
-console.log = function() {};
-global.$ = window.$ = require('jquery');
-var rewire = require('rewire'),
-    jsdom = require('jsdom').jsdom,
-  	appModule = rewire('../ui/main.js');
-
-global.document = jsdom('<body></body>');
-
-describe('', function() {
-  it ('Did you attach the text function to $(this)?', function() {
-    expect(appModule.__get__('main')).to.match(/\$\( *[\'"].projects-button[\'"] *\).on\( *[\'"]click[\'"], *function *\( *\) \{((\n|.)*)\$\( *this *\).text\(.*\)((\n|.)*)\}\)/);
-  });
-
-  it ('Did you provide the text function a string?', function() {
-    expect(appModule.__get__('main')).to.match(/\$\( *[\'"].projects-button[\'"] *\).on\( *[\'"]click[\'"], *function *\( *\) \{((\n|.)*)\$\( *this *\).text\( *[\'"].*[\'"]\)((\n|.)*)\}\)/);
-  });
-});
-
 
 var app = express();
 app.use(morgan('combined'));
