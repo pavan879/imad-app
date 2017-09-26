f1e6d6cda44e7470703d2588673b2eacc2b3f760
pavan879 <30998102+pavan879@users.noreply.github.com>
2017-09-26 19:39:19 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,5 +1,3 @@
-
-
 console.log('Loaded!');
 
 var img=document.getElementById('ntt');
@@ -7,12 +5,6 @@ img.onclick=function()
 {
     img.style.marginleft='1000px';
 };
-
-function main(){
-    $('.center').hide();
-    $('.center').fadeIn(1000);
-}
-
 
 
 function main() {
@@ -25,59 +17,7 @@ function main() {
     $(this).text('Projects Viewed');
     
   });
-  
-  function myblog(x){
-      if(x=='p'){
-      window.open('/pavan');}
-      else if(x=='q'){
-      window.open('/raja');}
-      else if(x=='r'){
-      window.open('/yash');}
-  }
-  
- $(':input').click(function () {
-      var x= $(this).attr("value");
-       myblog(x);
-    });
 }
-
-
-
-
-
+   
 $(document).ready(main);
 
-/*var button=document.querySelector('input');
-button.onclick=function myblog()
-{
-  window.open('/pavan');
-  var j = x.valueOf();
-};*/
-
-/*button.onclick=function myblog(x)
-{
-  if(x=='p'){
-  window.open('/pavan');}
-  else if(x=='q'){
-     window.open('/raja'); 
-  }
-  else if(x=='r'){
-     window.open('/yash'); 
-  }
-  
-};*/
-
-/*$(':input').on('click',myblog);
-
-    switch(myblog(x))
-    {
-        case 'p':window.open('/pavan');
-            break;
-            
-        case 'q':window.open('/raja');
-            break;
-            
-        case 'r':window.open('/yash');
-            break;
-    
-    }*/
