9529ba79d15e343424e886d16c7755fcb8b3fbf7
pavan879 <30998102+pavan879@users.noreply.github.com>
2017-10-02 03:11:01 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -17,7 +17,7 @@ app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index2.html'));
 });
 
-var pool=new pool(config);
+var pool=new Pool(config);
 app.get('/test-db',function(req,res){
    pool.query('select * from test',function(err,result){
        if(err)
