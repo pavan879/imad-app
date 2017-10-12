e116e9580b7c17421bff1df9374736f7a999e0b1
pavan879 <30998102+pavan879@users.noreply.github.com>
2017-10-12 11:42:04 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -94,20 +94,6 @@ app.get('/ui/main.js', function (req, res) {
 });
 
 
-app.get('/pavan',function(req,res)
-{
-  res.sendFile(path.join(__dirname, 'ui', 'pavan.html'));  
-}
-);
-app.get('/raja',function(req,res)
-{
-   res.sendFile(path.join(__dirname, 'ui', 'raja.html'));   
-}
-);app.get('/yash',function(req,res)
-{
-   res.sendFile(path.join(__dirname, 'ui', 'yash.html'));   
-}
-);
 // Do not change port, otherwise your app won't run on IMAD servers
 // Use 8080 only for local development if you already have apache running on 80
 
