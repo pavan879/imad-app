9c7bd1fdaf8145d2dee5fd01116b991b97ab7388
pavan879 <30998102+pavan879@users.noreply.github.com>
2017-10-26 16:01:32 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -61,7 +61,7 @@ app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 
-app.get('/ui/locators.html', function (req, res) {
+app.get('/locators', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'locators.html'));
 });
 
